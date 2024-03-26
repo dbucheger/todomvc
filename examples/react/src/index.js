56a46cdb4222a9d612925976962b5f089de05ee3
David Bucheger <dbucheger@clubautomation.com>
2024-03-26 17:08:34 -0400

    Added Dreamcoat + React example

--- examples/react/src/index.js
+++ examples/react/src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { HashRouter, Route, Routes } from "react-router-dom";
 
 import { App } from "./todo/app";
-import "todomvc-app-css/index.css";
 
-render(
-    <HashRouter>
-        <Routes>
-            <Route path="*" element={<App />} />
-        </Routes>
-    </HashRouter>,
-    document.getElementById("root")
-);
+import "todomvc-app-css/index.css";
+import "../src/dreamcoat/ui-components-lib-wc.css";
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(<HashRouter>
+	<Routes>
+		<Route path="*" element={<App />} />
+	</Routes>
+</HashRouter>);
